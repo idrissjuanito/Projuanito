ff9f7f990e6f3d2f98cd84f870db3ca79a2016c5
idrissjuanito <idrissjuanito@users.noreply.github.com>
2022-05-10 11:15:59 +0000

    deploy: a34cd09096b8f53966d8361f3a569f75411dac98

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import load from "./load.js";
+// const load = require("./load.js");
+// import hashChange from "./Router/loadView.js";
+
+// addEventListener("hashchange", hashChange);
+(async function main(){
+    load();
+})();
