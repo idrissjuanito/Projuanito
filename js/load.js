165bb57ed94492f8d31bb7e030fe3e48c024d59b
idrissjuanito <idrissjuanito@users.noreply.github.com>
2022-08-16 23:52:02 +0000

    deploy: 29140e9d63511661115a81bd3dd570dbfeb2974a

---
+++ js/load.js
@@ -0,0 +1,21 @@
+import {windowHeight, menuHamburger, fixedNav, scrollBackTop} from "./navigations.js";
+
+const sections = document.querySelectorAll('section');
+
+function scrollReveal(){
+
+    for(let section of sections){
+        const boundTop = section.getBoundingClientRect().top;
+        if( boundTop < windowHeight - 200){
+            const container = section.querySelector(".container");
+            container.classList.add('reveal');
+        }
+    }
+    fixedNav();
+    scrollBackTop();
+}
+
+export default () => {
+    document.addEventListener('scroll', scrollReveal);
+    menuHamburger;
+}
