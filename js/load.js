6b159caa0ff7045120138ebc40ca52cffacfb4ed
idrissjuanito <idrissjuanito@users.noreply.github.com>
2023-02-23 20:21:59 +0000

    deploy: b744dc24426c6604a74a4f6766a257eb1f258515

--- js/load.js
+++ js/load.js
@@ -1,21 +1,20 @@
-import {windowHeight, menuHamburger, fixedNav, scrollBackTop} from "./navigations.js";
+import { windowHeight, scrollBackTop, fixedNav } from "./navigations.js";
 
-const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll("section");
 
-function scrollReveal(){
-
-    for(let section of sections){
-        const boundTop = section.getBoundingClientRect().top;
-        if( boundTop < windowHeight - 200){
-            const container = section.querySelector(".container");
-            container.classList.add('reveal');
-        }
+function scrollReveal() {
+  for (let section of sections) {
+    const boundTop = section.getBoundingClientRect().top;
+    if (boundTop < windowHeight - 200) {
+      const container = section.querySelector(".container");
+      container.classList.add("reveal");
     }
-    fixedNav();
-    scrollBackTop();
+  }
+  fixedNav();
+  scrollBackTop();
 }
 
 export default () => {
-    document.addEventListener('scroll', scrollReveal);
-    menuHamburger;
-}
+  document.addEventListener("scroll", scrollReveal);
+  // menuHamburger;
+};
--- js/navigations.js
+++ js/navigations.js
@@ -1,22 +1,24 @@
-const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector(".navbar")
+const navigation = document.querySelector(".nav")
 const goBackTop = document.querySelector(".go-back-top");
 const toolsSection = document.querySelector(".daily-tools");
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 const menuIcon = document.querySelector('.menu-hamburger');
 export const windowHeight = document.documentElement.clientHeight;
-export const menuHamburger = menuIcon.addEventListener('click', closeMobileNav);
+export const menuHamburger = menuIcon.addEventListener("click", closeMobileNav);
 
 function closeMobileNav(){
-    
-    navbar.classList.toggle('open');
-    
+
+    menuIcon.classList.toggle("is_active");
+    navigation.classList.toggle("open")
+
     const navLinks = document.querySelectorAll('.nav a');
     navLinks.forEach((link) => link.removeEventListener('click', closeMobileNav));
-    if(navbar.classList.contains("open")){
+    if(navigation.classList.contains("open")){
         navLinks.forEach((link) => link.addEventListener('click', closeMobileNav));
     }
 }
-
+//
 export function fixedNav(){
     const bodyTop = body.getBoundingClientRect().top;
 
@@ -27,15 +29,15 @@ export function fixedNav(){
     }
 }
 
-export function scrollBackTop(){
-    const bodyTop = body.getBoundingClientRect().top;
-    goBackTop.addEventListener("click", () => {
-        window.scrollTo(0, 0);
-    });
+export function scrollBackTop() {
+  const bodyTop = body.getBoundingClientRect().top;
+  goBackTop.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
 
-    if(bodyTop < -600){
-        goBackTop.style.bottom = "5%";
-    }else{
-        goBackTop.style.bottom = "-5%";
-    }
+  if (bodyTop < -600) {
+    goBackTop.style.bottom = "5%";
+  } else {
+    goBackTop.style.bottom = "-5%";
+  }
 }
